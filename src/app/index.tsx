319997a63f3cfb3bade521355e8b60ec0be75bcd
Alicia Munhoz <aliciamfc96@gmail.com>
2024-12-17 16:45:18 -0300

    feat: adição do componente button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { View } from "react-native";
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+      <Button isLoading>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
