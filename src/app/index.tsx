ecc84eab285cf98c35b1db02bcc117b71a215a1e
Alicia Munhoz <aliciamfc96@gmail.com>
2024-12-16 22:58:21 -0300

    feat: criação do componente de loading

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+    </View>
+  );
+}
