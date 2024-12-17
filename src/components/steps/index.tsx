066da5db75b141fcdd45ee83b9ebf16a4d5ac32f
Alicia Munhoz <aliciamfc96@gmail.com>
2024-12-16 23:25:28 -0300

    feat: adição do componente steps

---
+++ src/components/steps/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { s } from "./styles";
+
+export function Steps() {
+  return (
+    <View style={s.container}>
+      <Text style={s.title}>Veja como funciona:</Text>
+    </View>
+  );
+}
